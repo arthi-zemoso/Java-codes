6d0e991b74d129fd66827ad9406e8d3225e00a68
ARthi ANeel <arthianeel2000@gmail.com>
2022-02-08 16:31:26 +0530

    made changes to assignment2

--- src/JavaAssignments/Assignment2.java
+++ src/JavaAssignments/Assignment2.java
@@ -1,39 +1,33 @@
  package JavaAssignments;
-
+import java.util.*;
 import java.util.Scanner;
 
-public class Assignment2 {            //the time complexity is O(26)
-    public void containsAllAlphabets(String input){
-        int count=0;
-        if(input.length()<26) {
-            System.out.println("Doesn't contain all alphabets a-z");
-            return;
-        }
-        String alphabet="abcdefghijklmnopqrstuvwxyz";
-        for(int index=0;index<alphabet.length();index+=1)
+public class Assignment2 {            //the time complexity is O(1)
+    public boolean containsAllAlphabets(String input){
+        HashMap<Character, Integer> map = new HashMap<>() ;     //Using a hashmap for counting the characters.
+        int count = 0 ;
+        for(int i = 0; i<input.length() ; i++)
         {
-            char currentChar=alphabet.charAt(index);
-            if(input.indexOf(currentChar)!=-1) {
-                count++;
-                continue;
-            }
+            count++ ;
+            if(map.containsKey(input.charAt(i)))
+                map.put(input.charAt(i) , map.get(input.charAt(i)) + 1) ;
             else
-                break;
+                map.put(input.charAt(i) , 1) ;
         }
-        if(count==26)
-        {
-            System.out.println("Contains all alphabets a-z");
-        }
-        else
-            System.out.println("doesn't contain all alphabets a-z");
-
+        for(char ch = 97 ; ch <=122 ;ch++ )
+            if(map.get(ch) == null)             //If any lowercase character didnt occur , return false
+                return false;
+        return true ;
     }
     public static void main(String[] args) {
         Assignment2 object=new Assignment2();
         Scanner keyboard=new Scanner(System.in);
         System.out.println("Enter your input");
         String input= keyboard.nextLine();
-        object.containsAllAlphabets(input);
+        if(object.containsAllAlphabets(input))
+            System.out.println("THE GIVEN STRING HAS ALL THE LOWER CASE CHARACTERS IN IT");
+        else
+            System.out.println("THE GIVEN STRING DOES NOT HAVE ALL THE LOWER CASE CHARACTERS IN IT");
     }
 
 }
