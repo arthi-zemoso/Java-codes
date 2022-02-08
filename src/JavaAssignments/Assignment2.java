fe66928c319980c8e2bec11188f16ec571c20a5f
ARthi ANeel <arthianeel2000@gmail.com>
2022-02-08 17:21:23 +0530

    made changes

--- src/JavaAssignments/Assignment2.java
+++ src/JavaAssignments/Assignment2.java
@@ -2,22 +2,30 @@
 import java.util.*;
 import java.util.Scanner;
 
-public class Assignment2 {            //the time complexity is O(1)
-    public boolean containsAllAlphabets(String input){
-        HashMap<Character, Integer> map = new HashMap<>() ;     //Using a hashmap for counting the characters.
-        int count = 0 ;
-        for(int i = 0; i<input.length() ; i++)
-        {
-            count++ ;
-            if(map.containsKey(input.charAt(i)))
-                map.put(input.charAt(i) , map.get(input.charAt(i)) + 1) ;
-            else
-                map.put(input.charAt(i) , 1) ;
+public class Assignment2 {            //the time complexity is O(N) where n is length of string
+    public boolean containsAllAlphabets(String input) {
+        Map<Character, Boolean> map = new HashMap<>();
+        char temp = ' ';
+        int countLetter = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) >= 'A' && input.charAt(i) <= 'Z') { // converting to lower case
+                temp = (char) (input.charAt(i) + 32);
+            }
+            else if (input.charAt(i) >= 'a' && input.charAt(i) <= 'z')
+            {
+                temp = input.charAt(i);
+            }
+            else { // skip character other than alphabets continue;
+            }
+            if (!map.containsKey(temp)) {
+                map.put(temp, true);
+                countLetter++;
+            }
         }
-        for(char ch = 97 ; ch <=122 ;ch++ )
-            if(map.get(ch) == null)             //If any lowercase character didnt occur , return false
-                return false;
-        return true ;
+        if (countLetter == 26) {
+            return true;
+        }
+        return false;
     }
     public static void main(String[] args) {
         Assignment2 object=new Assignment2();
@@ -25,9 +33,9 @@ public class Assignment2 {            //the time complexity is O(1)
         System.out.println("Enter your input");
         String input= keyboard.nextLine();
         if(object.containsAllAlphabets(input))
-            System.out.println("THE GIVEN STRING HAS ALL THE LOWER CASE CHARACTERS IN IT");
+            System.out.println("THE GIVEN STRING HAS ALL THE LETTERS OF THE ALPHABET  IN IT");
         else
-            System.out.println("THE GIVEN STRING DOES NOT HAVE ALL THE LOWER CASE CHARACTERS IN IT");
+            System.out.println("THE GIVEN STRING DOES NOT HAVE ALL THE LETTERS OF THE ALPHABETS IN IT");
     }
 
 }
